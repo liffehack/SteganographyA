ff23bb7a1a817fa860532536feb079e61ea161ef
liffehack <M1qa2ws3ed4rfoD>
2020-06-04 19:50:49 +0300

    Добавьте файлы проекта.

---
+++ SteganographyA/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace SteganographyA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(27, 64);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(329, 102);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(27, 260);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(329, 102);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(596, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "1.расшифровать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(596, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "2.зашифровать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(596, 134);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(271, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(593, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Сообщение";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(214, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Емкость";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(362, 260);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(329, 102);
+            this.richTextBox3.TabIndex = 9;
+            this.richTextBox3.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+    }
+}
+
---
+++ SteganographyA/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SteganographyA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            richTextBox1.Text = "Слова и фразы 1. Слова и фразы 2. Слова и фразы 3! Слова и фразы 4? Слова и фразы 5. Слова и фразы 6. Слова и фразы 7. Слова и фразы 8! Слова и фразы 9? Слова и фразы 10.";
+            label1.Text = (richTextBox1.Text.Split('.', '!', '?').Length - 1).ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = richTextBox1.Text + "#";
+            label1.Text = (str.Split('.','!', '?').Length-1).ToString();
+            char[] chars = str.ToCharArray();
+            
+            string msg = "";
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                
+                if (chars[i] == '.' || chars[i] == '!' || chars[i] == '?')
+                {
+                   
+                    if (chars[i] == '#') break;
+                    if(chars[i + 1]==' ' && chars[i + 2]==' ')
+                    {
+                        msg += "0";
+                    }
+                    if (chars[i + 1] == ' ' && chars[i + 2] != ' ')
+                    {
+                        msg += "1";
+                    }                   
+                }
+            }
+            MessageBox.Show(msg + " " + (char)Convert.ToInt32(msg, 2));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string str = richTextBox1.Text;
+            label1.Text = (str.Split('.', '!', '?').Length - 1).ToString();
+            byte[] asciiBytes = Encoding.ASCII.GetBytes(textBox1.Text);           
+            char[] msgChars = (Convert.ToString(asciiBytes[0], 2)+"#").ToCharArray();
+            string msgD ="";
+            for (int i=0; i< msgChars.Length;i++)
+            {  msgD += msgChars[i]; }          
+            MessageBox.Show(msgD);
+
+            
+            str = str.Replace("  ", " ");
+           // str = str.Replace("", "@");
+            string text = "";
+
+            char[] chars = str.ToCharArray();           
+            List<Char> indexs = new List<Char>();
+            List<string> subStrs = new List<string>();           
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] == '.' || chars[i] == '!' || chars[i] == '?')
+                {                   
+                    indexs.Add(chars[i]);
+                }
+            }
+            subStrs.AddRange(str.Split('.', '!', '?'));
+            for(int i = 1; i < subStrs.Count; i++)
+            {
+                subStrs[i] = subStrs[i].TrimStart(' ');
+            }
+
+            for (int i = 0; i < indexs.Count; i++)
+            {
+                subStrs[i] = subStrs[i] + indexs[i];                
+            }
+            int last = 0;
+            for (int i = 0; i < msgChars.Length; i++)
+            {
+                if (msgChars[i] == '#')
+                    richTextBox3.Text += subStrs[i] + '#';
+                if (msgChars[i] == '1')
+                    richTextBox3.Text += subStrs[i] + " ";
+                if (msgChars[i] == '0')
+                    richTextBox3.Text += subStrs[i] + "  ";
+                //
+                subStrs[i] = subStrs[i] + msgChars[i];
+                text += subStrs[i];
+                last = i;
+
+                
+            }
+            for (int i = last+1; i < indexs.Count; i++)
+            {
+               // subStrs[i] = subStrs[i];
+                text += subStrs[i];
+                richTextBox3.Text += subStrs[i];
+            }
+
+            richTextBox2.Text = text ;
+
+
+        }
+    }
+}
